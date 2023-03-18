ff85aa065b8e297a5ad07ed869c5eceaee094adf
AlvinaDmy <alvinadmynti0204@gmail.com>
2023-03-18 22:59:24 +0700

    lab2_php_dasar

---
+++ php_dasar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+
+    <h1>Menggunakan variabel</h1>
+    <?php
+    $nim = "312110125";
+    $nama = 'Alvina Damayanti';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+    
+</body>
+</html>
